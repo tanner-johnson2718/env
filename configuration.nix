b68bfeb258ff33f7e6744d999e4a4004c1e78b00
Tanner Johnson <tanner.johnson2718@gmail.com>
2024-07-20 15:54:02 -0500

    ..

--- configuration.nix
+++ configuration.nix
@@ -142,4 +142,7 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
 
+  # Enable Flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
 }
