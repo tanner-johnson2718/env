e6cd53d10fb18107351d5a3675fbc1a0a12d9d31
LCARS <tanner.johnson2718@gmail.com>
2024-09-01 18:15:50 -0700

    AUTO COMMIT

--- qmk/qmk.nix
+++ qmk/qmk.nix
@@ -1,4 +1,4 @@
-{pkgs}:
+{pkgs, keymap}:
 let
   qmk_firmware_url = "https://github.com/qmk/qmk_firmware.git";
   qmk_firmware_rev = "fe50774cb488f981dfd41c7a5bc5f34dfcd9cb9d";
@@ -7,12 +7,14 @@ in
 pkgs.mkShell {
   packages = with pkgs; [ git qmk ];
   shellHook = ''
-    echo "Cloning QMK Firmware Source..."
-    git clone ${qmk_firmware_url}
+    if ! [ -d qmk_firmware ]; then
+      echo "Cloning QMK Firmware Source..."
+      git clone ${qmk_firmware_url}
+    fi
     cd qmk_firmware
     git checkout ${qmk_firmware_rev}
     qmk setup
-    cp ../keymap.c ${keymap_path}
+    cp ${keymap} ${keymap_path}
     qmk compile --keyboard gmmk/gmmk2/p65/ansi --keymap via
     sudo qmk flash --keyboard gmmk/gmmk2/p65/ansi --keymap via
   '';
