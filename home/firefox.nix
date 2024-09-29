66c7718434387d4ee2924983225d1a338c5aac4b
none <none@none>
2024-09-28 17:01:44 -0700

    ..

--- home/firefox.nix
+++ home/firefox.nix
@@ -1,6 +1,23 @@
-{ pkgs, home, config, ... }:
+# https://github.com/yokoffing/Betterfox/blob/main/user.js
+# https://github.com/oddlama/nix-config/blob/main/users/myuser/graphical/firefox.nix
+{ pkgs, home, config, lib, ... }:
+let
+  betterfox = pkgs.fetchFromGitHub {
+    owner = "yokoffing";
+    repo = "Betterfox";
+    rev = "129.0";
+    hash = "sha256-hpkEO5BhMVtINQG8HN4xqfas/R6q5pYPZiFK8bilIDs=";
+  };
+in
 {
     programs.firefox = {
         enable = true;
+        profiles ={
+          default = {
+            extraConfig =  builtins.concatStringsSep "\n" [ 
+              (builtins.readFile "${betterfox}/user.js")
+            ];
+          };
+        };
     };
 }
--- home/vscode.nix
+++ home/vscode.nix
@@ -4,5 +4,10 @@
         enable = true;
         enableUpdateCheck = false;
         package = pkgs.vscodium;
+        extensions = with pkgs; [
+            vscode-extensions.streetsidesoftware.code-spell-checker
+            vscode-extensions.bbenoist.nix
+            vscode-extensions.jnoortheen.nix-ide
+        ];
     };
 }
