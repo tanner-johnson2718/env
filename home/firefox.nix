650096e3ecdd7702269ff942b42949bd3675408c
none <none@none>
2024-09-28 17:55:56 -0700

    ..

--- home/firefox.nix
+++ home/firefox.nix
@@ -16,6 +16,25 @@ in
           default = {
             extraConfig =  builtins.concatStringsSep "\n" [ 
               (builtins.readFile "${betterfox}/user.js")
+              (''
+                user_pref("services.sync.prefs.sync-seen.privacy.clearOnShutdown.cookies", true);
+                user_pref("services.sync.prefs.sync-seen.privacy.clearOnShutdown_v2.cookiesAndStorage", true);
+                user_pref("privacy.clearOnShutdown.cache", true);
+                user_pref("privacy.clearOnShutdown.cookies", true);
+                user_pref("privacy.clearOnShutdown.downloads", true);
+                user_pref("privacy.clearOnShutdown.formdata", true);
+                user_pref("privacy.clearOnShutdown.history", true);
+                user_pref("privacy.clearOnShutdown.sessions", true);
+                user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", true);
+                user_pref("dom.webnotifications.enabled",			false);
+                user_pref("browser.urlbar.placeholderName", "DuckDuckGo");
+                user_pref("privacy.sanitize.pending", "[{\"id\":\"shutdown\",\"itemsToClear\":[\"cache\",\"siteSettings\",\"historyFormDataAndDownloads\",\"cookiesAndStorage\"],\"options\":{}}]");
+                user_pref("privacy.sanitize.sanitizeOnShutdown", true);
+                user_pref("privacy.donottrackheader.enabled", true);
+                user_pref("privacy.clearOnShutdown_v2.siteSettings", true);
+                user_pref("extensions.formautofill.addresses.enabled", false);
+                user_pref("extensions.formautofill.creditCards.enabled", false);
+              '')
             ];
           };
         };
